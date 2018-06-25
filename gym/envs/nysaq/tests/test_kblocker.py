9e181a7d45dcf14c9418abb70cee14d89aab6e4f
BruceK4t1qbit <bruce.krayenhoff@1qbit.com>
2018-06-25 16:26:30 -0700

    Refactored test_kblocker:
    Extracted environment setup boilerplate from all tests to a function.

--- gym/envs/nysaq/tests/test_kblocker.py
+++ gym/envs/nysaq/tests/test_kblocker.py
@@ -3,28 +3,29 @@ import numpy as np
 from gym.envs.nysaq import KBlockerEnv
 from gym.spaces import Box, Discrete
 
+
+def get_k_blocker_env(fileName):
+    config_folder = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    config_path = config_folder + '/assets/' + fileName
+    task = KBlockerEnv(config_path)
+    return task
+
+
 @pytest.mark.parametrize("fileName,observations", [
     ('rbmConfig1bt', Box(np.array([0,0]),np.array([20,20]))),
     ('rbmConfig2bt', Box(np.array([0,0,0]),np.array([28,28,28])))
 ])
 def test_observation(fileName, observations):
-    config_folder = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-    config_path = config_folder + '/assets/' + fileName
-
-    task = KBlockerEnv(config_path)
-
+    task = get_k_blocker_env(fileName)
     assert task.observation_space == observations
+
 
 @pytest.mark.parametrize("fileName,actions", [
     ('rbmConfig1bt', Discrete(16)),
     ('rbmConfig2bt', Discrete(64))
 ])
 def test_actions(fileName, actions):
-
-    config_folder = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-    config_path = config_folder + '/assets/' + fileName
-    task = KBlockerEnv(config_path)
-
+    task = get_k_blocker_env(fileName)
     assert task.action_space == actions
 
 # normal walk,
@@ -32,18 +33,16 @@ def test_actions(fileName, actions):
 # avoiding running each other over.
 # avoiding going into the blockers
 @pytest.mark.parametrize("cc, action, nc", [
-    ([(0,0),(3,0)], 4, [(1,0),(2,0)]),#action: [0,1]
-    ([(0,0),(3,0)], 1, [(0,0),(3,0)]),#action: [1,0]
-    ([(0,0),(3,0)], 14, [(0,0),(3,1)]),#action: [2,3]
-    ([(1,1),(2,1)], 4, [(1,1),(2,1)]),#action: [0,1]
-    ([(0,3),(3,3)], 15, [(0,3),(3,4)]) #action: [3,3] The blocker is on the left
+    ([(0,0),(3,0)], 4, [(1,0),(2,0)]),  #action: [0,1]
+    ([(0,0),(3,0)], 1, [(0,0),(3,0)]),  #action: [1,0]
+    ([(0,0),(3,0)], 14, [(0,0),(3,1)]),  #action: [2,3]
+    ([(1,1),(2,1)], 4, [(1,1),(2,1)]),  #action: [0,1]
+    ([(0,3),(3,3)], 15, [(0,3),(3,4)])  #action: [3,3] The blocker is on the left
 ])
 def test_coords(cc, action, nc):
     fileName = 'rbmConfig1bt'
-    config_folder = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-    config_path = config_folder + '/assets/' + fileName
+    task = get_k_blocker_env(fileName)
 
-    task = KBlockerEnv(config_path)
     task._KBlockerEnv__coords = cc
     task._KBlockerEnv__gap = task._KBlockerEnv__grid_x - 1
     task.step(action)
@@ -55,13 +54,11 @@ def test_coords(cc, action, nc):
 ])
 def test_winning(cc, action, exp_rew, exp_done):
     fileName = 'rbmConfig1bt'
-    config_folder = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-    config_path = config_folder + '/assets/' + fileName
+    task = get_k_blocker_env(fileName)
 
-    task = KBlockerEnv(config_path)
     task._KBlockerEnv__coords = cc
     task._KBlockerEnv__gap = task._KBlockerEnv__grid_x - 1
-    _, rew, done, _ = task.step(action)
+    _, rew, done, _y = task.step(action)
     assert rew == exp_rew
     assert done == exp_done
 
@@ -72,10 +69,8 @@ def test_winning(cc, action, exp_rew, exp_done):
 ])
 def test_defending(cc, action, exp_rew, exp_done):
     fileName = 'rbmConfig1bt'
-    config_folder = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-    config_path = config_folder + '/assets/' + fileName
+    task = get_k_blocker_env(fileName)
 
-    task = KBlockerEnv(config_path)
     task._KBlockerEnv__coords = cc
     task._KBlockerEnv__gap = task._KBlockerEnv__grid_x - 1
     _, rew, done, _ = task.step(action)
